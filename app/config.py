b6f26335ed6031032aea0871b48a8daa9373b726
CelebriChain User <ym@ahimsa.coop>
2025-05-05 15:18:43 +0300

    Fix local DB URI, init migrations, add discount script

--- app/config.py
+++ app/config.py
@@ -1,6 +1,9 @@
 import os
 from datetime import timedelta
 
+# Define base directory for the project
+BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
 
 class Config:
     SECRET_KEY = (
@@ -10,9 +13,7 @@ class Config:
     # Шлях до бази даних - тут ми використовуємо абсолютний шлях для PythonAnywhere
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL"
-    ) or "sqlite:////" + os.path.join(
-        os.path.dirname(os.path.abspath(__file__)), "../beauty_salon.db"
-    )
+    ) or "sqlite:///" + os.path.join(BASE_DIR, "instance", "beauty_salon.db")
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     REMEMBER_COOKIE_DURATION = timedelta(days=14)
